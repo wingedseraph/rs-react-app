424607360316bc430e4b6688b9d29297b23bcc15
wingedseraph <r89647@gmail.com>
2025-07-17 17:24:37 +0300

    feat: add vitest package to project and example of test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -11,5 +12,25 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
     'process.env': {},
+  },
+  test: {
+    coverage: {
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+    },
   },
 });
