93dc808f68a4ac4dbdc3a6e64a807daa24667b1f
wingedseraph <r89647@gmail.com>
2025-07-27 01:44:02 +0300

    feat: implement custom hook: useLocalStorage

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -6,7 +6,7 @@ export const Layout = () => {
   return (
     <>
       <header className="transition-all md:w-[40%] m-auto hover:scale-110">
-        <img src="/logo.png" />
+        <img src="/logo.png" alt="pokemon logo" />
       </header>
       <main className="transition-all mt-20">
         <Suspense fallback={<Spinner />}>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,23 +1,20 @@
 import { getPokemonByQuery } from '@/api/getPokemonByQuery';
 import { CardList } from '@/components/CardList';
 import { Search } from '@/components/Search';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { CONST, type Card } from '@/types';
 import { useEffect, useState } from 'react';
 
 function Index() {
-  const [value, setValue] = useState(
-    localStorage.getItem(CONST.POKEMON_QUERY) ?? ''
-  );
+  const [value, setValue] = useLocalStorage('', CONST.POKEMON_QUERY);
   const [pokemonCards, setPokemonCards] = useState<Card[]>([]);
   const [loading, setLoading] = useState(true);
 
   const onSearch = async () => {
     const query = value.trim();
-    localStorage.setItem(CONST.POKEMON_QUERY, query);
     setLoading(true);
 
     const responseData: Card[] = await getPokemonByQuery(query);
-    console.warn('PokemonResponse: ', responseData);
 
     if (responseData) {
       setPokemonCards(responseData);
