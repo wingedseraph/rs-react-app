1c12b901d0c3a764eb24beeaf600dbb49af33bb9
wingedseraph <r89647@gmail.com>
2025-07-27 01:44:28 +0300

    feat: implement custom hook useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,16 @@
+import { CONST } from '@/types';
+import { useEffect, useState, type Dispatch, type SetStateAction } from 'react';
+
+export const useLocalStorage = (
+  default_: string,
+  key: string
+): [string, Dispatch<SetStateAction<string>>] => {
+  const [state, setState] = useState(() => {
+    return localStorage.getItem(CONST.POKEMON_QUERY) ?? default_;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, state);
+  }, [state, key]);
+  return [state, setState];
+};
