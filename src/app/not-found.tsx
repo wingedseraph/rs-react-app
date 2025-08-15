ea129523598acf45ca0d281fe7cce28fff288075
wingedseraph <r89647@gmail.com>
2025-08-15 14:57:32 +0300

    fix: tailwind work with nextjs

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function NotFound(): React.ReactNode {
   return (
     <div className="flex flex-col items-center gap-2 rounded-xl p-10 text-3xl">
-      <h1 className="mb-10 bg-red-500 text-4xl font-bold">page not found</h1>
+      <h1 className="mb-10 text-4xl font-bold">page not found</h1>
       <Link className="transition-all" href={Path.index}>
         ← to index page
       </Link>
