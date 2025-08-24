c22545ad17e84e0f7eedb8029a39575672495342
wingedseraph <r89647@gmail.com>
2025-08-25 02:07:34 +0300

    feat: add password strength util

--- src/shared/ui/PasswordStrength/PasswordStrength.tsx
+++ src/shared/ui/PasswordStrength/PasswordStrength.tsx
@@ -1,34 +1,12 @@
-import { formSchema } from '@/lib/validation/formSchema';
+import { getStrength } from '@/shared/utils/getStrenth';
 
-type PasswordStrengthProps = {
-  password: string;
-};
-
-export default function PasswordStrength({ password }: PasswordStrengthProps) {
-  const getStrength = (pass: string) => {
-    if (!pass) return { level: 0, text: 'too short' };
-
-    let score = 0;
-
-    const result = formSchema.shape.password.safeParse(password);
-    const maxScore = 6;
-
-    score = result.success
-      ? maxScore
-      : Math.max(0, maxScore - result.error.issues.length);
-
-    if (score <= 4) return { level: score, text: 'weak' };
-    if (score <= 5) return { level: score, text: 'good' };
-
-    return { level: score, text: 'strong' };
-  };
-
+export default function PasswordStrength({ password }: { password: string }) {
   const strength = getStrength(password);
   const width = password ? Math.min((strength.level / 6) * 100, 100) : 0;
 
   return (
     <div>
-      <div className="flex justify-between text-xs text-gray-600">
+      <div className="flex justify-between text-base">
         <span className="text-secondary">{strength.text}</span>
       </div>
       <div className="bg-border-primary mt-1 h-1 w-full rounded">
