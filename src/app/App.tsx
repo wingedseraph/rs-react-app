eff3e18455b6c8064405fcb7d571d8a2757b0771
wingedseraph <r89647@gmail.com>
2025-08-26 00:26:36 +0300

    feat: add basic husky scripts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
             </tr>
           </tbody>
         </Table>
-        {/*todo: <CustomCursor /> */}
+        <CustomCursor />
       </Layout>
     </>
   );
