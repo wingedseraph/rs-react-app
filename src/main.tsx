5231013005490e01b87abf0d1e486c9d01e89f50
wingedseraph <r89647@gmail.com>
2025-08-31 04:25:02 +0300

    chore: apply prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
-
 import { createRoot } from "react-dom/client";
 
 import App from "@/app/App";
+
 import "./index.css";
 
 const rootElement = document.getElementById("root");
